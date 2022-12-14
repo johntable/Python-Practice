8af98833d9ebeb547af11568b78ce3f3de1c87e9
johntable <john.m.tabelisma@gmail.com>
2022-12-14 17:24:46 -0600

    new programs added

--- foot_the_bill.py
+++ foot_the_bill.py
@@ -7,7 +7,10 @@ names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
-rand_lim = len(names) - 1
-index = random.randint(0, rand_lim)
-payer = names[index]
+# rand_lim = len(names) - 1
+# index = random.randint(0, rand_lim)
+# payer = names[index]
+# print(f"{payer} is going to buy the meal today!")
+
+payer = random.choice(names)
 print(f"{payer} is going to buy the meal today!")
---
+++ rock_paper_scissors.py
@@ -0,0 +1,51 @@
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+# Write your code below this line 👇
+import random
+
+signs = [rock, paper, scissors]
+p_choice = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n")
+)
+player = signs[p_choice]
+print(player)
+
+
+opponent = random.choice(signs)
+print(f"Computer chose: {opponent}")
+
+if (
+    ((player == rock) and (opponent == scissors))
+    or ((player == scissors) and (opponent == paper))
+    or ((player == paper) and (opponent == rock))
+):
+    print("You win")
+elif player == opponent:
+    print("It's a draw")
+else:
+    print("You lost.")
