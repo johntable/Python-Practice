8d688cc4b4cca9a7d10e5f195c0931f38841f4a7
johntable <john.m.tabelisma@gmail.com>
2024-11-04 10:43:24 -0600

    Bug fixes and improvements to number guessing game

--- guess_num/main.py
+++ guess_num/main.py
@@ -2,13 +2,13 @@ from guess_art import logo
 import os, random
 modes = ['easy','hard']
 
-def is_number(num):
-    num
-
+# Game function
 def game():
+    # Compares the guess number with the true number
     def compare(guess_num, tru_num):
         if (guess_num == tru_num):
             print(f'You got it! {guess_num} is my number: {tru_num}')
+            print(f'You had {guesses} remaining')
             return -guesses
         elif(guess_num > tru_num):
             print(f'Guess: {guess_num}. Your guess is too high.')
@@ -17,42 +17,40 @@ def game():
             print(f'Guess: {guess_num}. Your guess is too low.')
             return -1
 
+    # Gives the user 10 or 5 guesses based on difficulty
     def choose_mode(mode):
         if mode == 'easy':
             return guesses + 11
         elif mode == 'hard':
             return guesses + 6
-
+        
+    # Guesses = -1 for debugging
     guesses = -1
     the_num = random.randint(1,100)
     print(logo)
 
-    difficulty = input('''
-Welcome to the Number Guessing Game!!!
+    # Difficulty choice section
+    difficulty = input('''Welcome to the Number Guessing Game!!!
 Choose a difficulty. Type \'easy\' or \'hard\': 
 ''').lower()
-    
     if difficulty not in modes:
-        difficulty = input('''
-Invalid Input.
+        difficulty = input('''Invalid Input.
 Welcome to the Number Guessing Game!!!
 Choose a difficulty. Type \'easy\' or \'hard\': 
 ''').lower()
-    
     guesses = choose_mode(difficulty)
-    print(guesses)
-    print(the_num)
 
+    # Game loop
     while guesses != 0:
         ur_guess = int(input(f'\tThe number is between 1 and 100\n\tNumber of guesses remaining: {guesses}\n\tType your guess: '))
         os.system('clear')
         print(logo)
         guesses += compare(ur_guess,the_num)
         if guesses == 0 and (ur_guess != the_num):
-            print('You\'ve run out of guesses\n Better luck next time!')
+            print('You\'ve run out of guesses.')
+            print(f'{the_num} is the number. Better luck next time!')
 
-
-
+# main
 while input('Do you want to play? \'y\' or \'n\': ') == 'y':
     os.system('clear')
     game()
