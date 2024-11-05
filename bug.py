ab71b0ab14ca57facbe09697d4327a6fe7fa6e22
johntable <john.m.tabelisma@gmail.com>
2024-11-05 12:41:33 -0600

    age group bug problem

---
+++ bug.py
@@ -0,0 +1,8 @@
+year = int(input('What\'s the year of your birth? '))
+
+if (1980 <= year < 1994):
+    print('You are a millenial. How\'s that college debt going? Paid it off yet?')
+elif 1994 <= year:
+    print('You are Gen Z or younger. We both know your depressed, so I don\'t need to roast you.')
+else:
+    print('You old. Do you need a rotary to phone a happier time?')
