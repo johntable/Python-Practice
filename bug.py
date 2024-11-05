807f62d9361cb258cb08271e712718bb6dfe16a5
johntable <john.m.tabelisma@gmail.com>
2024-11-05 13:02:05 -0600

    legal age finder bug

--- bug.py
+++ bug.py
@@ -1,8 +1,7 @@
-year = int(input('What\'s the year of your birth? '))
-
-if (1980 <= year < 1994):
-    print('You are a millenial. How\'s that college debt going? Paid it off yet?')
-elif 1994 <= year:
-    print('You are Gen Z or younger. We both know your depressed, so I don\'t need to roast you.')
-else:
-    print('You old. Do you need a rotary to phone a happier time?')
+try: 
+    age = int(input('What is your age? '))
+except ValueError:
+    print('Invalid Input. Please enter a number.')
+    age = int(input('What is your age? '))
+if age > 18:
+    print(f'You can drive, drink, go to jail (preferably not at the same time) at age {age}')
